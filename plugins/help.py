cd8a802d3f5d08074b66a834792afe5e5878f398
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-09 10:47:03 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"🤗Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url🤓 🤡 **You Can youtube video links 🗣️search using @bidd inline mode🤗**   ❤️❤️A Bot By @omindas"
     await message.reply_text(helptxt)
