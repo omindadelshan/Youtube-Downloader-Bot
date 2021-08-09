be5c1658c52f91399c617cb9761cd67656ca57eb
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-09 11:25:59 +0530

    Update youtube_callback_data.py

--- plugins/youtube_callback_data.py
+++ plugins/youtube_callback_data.py
@@ -25,11 +25,11 @@ async def catch_youtube_fmtid(c, m):
         print(media_type)
         if media_type == 'audio':
             buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
-                "Audio", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("Document",
+                "🎸Audio🎸", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("Document",
                                                                                                     callback_data=f"docaudio||{format_id}||{yturl}")]])
         else:
             buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
-                "Video", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("Document",
+                "🎬Video🎬", callback_data=f"{media_type}||{format_id}||{yturl}"), InlineKeyboardButton("Document",
                                                                                                     callback_data=f"docvideo||{format_id}||{yturl}")]])
 
         await m.edit_message_reply_markup(buttons)
@@ -101,7 +101,7 @@ async def catch_youtube_dldata(c, q):
     loop = asyncio.get_event_loop()
 
     med = None
-    if cb_data.startswith("audio"):
+    if cb_data.startswith("🎸audio🎸"):
         filename = await downloadaudiocli(audio_command)
         med = InputMediaAudio(
             media=filename,
@@ -110,7 +110,7 @@ async def catch_youtube_dldata(c, q):
             title=os.path.basename(filename)
         )
 
-    if cb_data.startswith("video"):
+    if cb_data.startswith("🎬video🎬"):
         filename = await downloadvideocli(video_command)
         dur = round(duration(filename))
         med = InputMediaVideo(
@@ -149,7 +149,7 @@ async def send_file(c, q, med, filename):
     print(med)
     try:
         await q.edit_message_reply_markup(
-            InlineKeyboardMarkup([[InlineKeyboardButton("Uploading...", callback_data="down")]]))
+            InlineKeyboardMarkup([[InlineKeyboardButton("📤Uploading...📤", callback_data="down")]]))
         await c.send_chat_action(chat_id=q.message.chat.id, action="upload_document")
         # this one is not working
         await q.edit_message_media(media=med)
